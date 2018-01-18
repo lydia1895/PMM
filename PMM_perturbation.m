5397bab891f451597e443cc4e116ced7ef464fe3
Lydia <lydia.pogorelskaya@gmail.com>
2018-01-18 17:09:30 +0300

    start for perturbation

--- PMM_perturbation.m
+++ PMM_perturbation.m
@@ -1,5 +1,8 @@
-function [W_perturb, pplus_perturb, pminus_perturb, gammaminus_perturb] = ...
-        PMM_perturbation(Lfull_eig, W_eig, gammasqr_eig, Lfull_perturb)
+function [H_perturb, gammasqr_perturb] = ...
+        PMM_perturbation(Lfull_eig, H_eig, gammasqr_eig, Lfull_perturb)
+    
+    delta_L = Lfull_perturb - Lfull_eig;
+    gammasqr_perturb = gammasqr_eig + diag(diag(transpose(H_eig)*delta_L*H_eig));
     
     
     
