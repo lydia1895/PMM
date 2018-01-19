e5ca373719fceef38025a778456f5a68abea76d4
Lydia <lydia.pogorelskaya@gmail.com>
2018-01-19 12:31:14 +0300

    progress

--- PMM_eig_for_Maxwell.m
+++ PMM_eig_for_Maxwell.m
@@ -1,8 +1,8 @@
 
 function [H_1_4,gamma_sqr_1_4]= ...
-        PMM_eig_for_Maxwell(L_HE,L_EH)
+        PMM_eig_for_Maxwell(L_full)
    
     
-    L_full = L_HE*L_EH;
+    %L_full = L_HE*L_EH;
     [H_1_4, gamma_sqr_1_4] = eig(L_full);
     
