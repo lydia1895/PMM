ed2db0705f6e5436d84a1951761dd7275980a54a
Lydia <lydia.pogorelskaya@gmail.com>
2018-01-22 13:19:09 +0300

    perturb with matrices faster version

--- eig_perturbation.m
+++ eig_perturbation.m
@@ -1,24 +1,24 @@
 function [H_perturb, gammasqr_perturb] = ...
         eig_perturbation(Lfull_eig, H_eig, gammasqr_eig, Lfull_perturb, N_total_3)
     
+    
     delta_L = Lfull_perturb - Lfull_eig;
     gammasqr_perturb = gammasqr_eig + diag(diag(transpose(H_eig)*delta_L*H_eig));
     
     
     H_L_H = transpose(H_eig)*delta_L*H_eig;
+    lambda_row = transpose(diag(gammasqr_eig));
+    lambda_matrix = repmat(lambda_row,2*N_total_3,1);
+    diff_lambda = lambda_matrix - transpose(lambda_matrix);
     
-    %H_L_H_divided = (H_L_H - diag(H_L_H))/
+    %%%small trick
+    diff_lambda = diff_lambda + eye(2*N_total_3,2*N_total_3);
+    H_L_H_divided = H_L_H./diff_lambda;
+    H_L_H_divided = H_L_H_divided - diag(diag(H_L_H_divided));
+    %%%
     
-    H_perturb = H_eig;
+    H_perturb = H_eig + H_eig*H_L_H_divided;
     
-    for i=1:2*N_total_3
-        for j = 1:2*N_total_3
-            if i~=j
-                H_perturb(:,i) = H_perturb(:,i) + H_eig(:,j)*...
-                    H_L_H(j,i)/(gammasqr_eig(i,i)-gammasqr_eig(j,j));
-            end
-        end
-    end
     
                 
                           
