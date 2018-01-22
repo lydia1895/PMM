f8b705eb5a79d517c5bbbe1d2922ac98abab4271
Lydia <lydia.pogorelskaya@gmail.com>
2018-01-22 16:24:43 +0300

    shorter epsilon

--- main_ellipse_plasmonic.m
+++ main_ellipse_plasmonic.m
@@ -23,7 +23,7 @@ lamnkSiO2 = MatParam_nk_SiO2_interpExportData;
 lamnkZep = MatParam_nk_Zep520A_interpExportData;
 
 Nlambda_eig = 2;
-n_lambda_extra_perturb = 3;
+n_lambda_extra_perturb = 7;
 Nlambda_perturb = n_lambda_extra_perturb * Nlambda_eig;
 half_n_lambda = floor((n_lambda_extra_perturb-1)/2);
 
@@ -38,7 +38,7 @@ Nphi_perturb = n_phi_extra_perturb * Nphi_eig;
 half_n_phi = floor((n_phi_extra_perturb-1)/2);
 
 lmin = 1200;
-lmax = 1280;
+lmax = 1240;
 
 %dlambda_eig = (lmax-lmin)/(Nlambda_eig-1);
 %dlambda_perturb = (lmax-lmin)/n_lambda_extra_perturb;
@@ -185,7 +185,7 @@ load('ellipse_plasmonic.mat','figure_shape', 'dispersion', 'lambda', 'theta', 'p
     'n_points', 'eta', 'f1', 'verbose')
 %}
 %calculate reflection and transmission
-[Rsum_1_4,Tsum_1_4, M_1_4, gammaminus_1_4] = ...
+[Rsum_p,Tsum_p, M_p, gammaminus_p] = ...
     PMM_main_function(figure_shape, dispersion, lambda, theta, phi, delta,...
     h, L, N_FMM, epsilon, refIndices, La, tau_x, tau_y, alpha_ref, beta_ref,...
     b_x1, b_x2, N_basis_x, N_basis_y, N_intervals_x, N_intervals_y,ellipse_parameters,...
@@ -199,11 +199,11 @@ for i=1:L
 end
     %}
     
-    save('ellipse_plasmonic_perturb_1order_output.mat','Rsum_1_4','Tsum_1_4', 'gammaminus_1_4')
-    load('ellipse_plasmonic_output.mat','Rsum_ellipses','Tsum_ellipses',...
-        'M', 'gammaminus');
-    difference_R_sum = abs(Rsum_1_4-Rsum_ellipses);
-    difference_T_sum = abs(Tsum_1_4-Tsum_ellipses);
+    save('ellipse_plasmonic_perturb_1order_output.mat','Rsum_p','Tsum_p', 'gammaminus_p')
+    %load('ellipse_plasmonic_output.mat','Rsum_ellipses','Tsum_ellipses',...
+    %    'M', 'gammaminus');
+    %difference_R_sum = abs(Rsum_1_4-Rsum);
+    %difference_T_sum = abs(Tsum_1_4-Tsum);
     %{
 [l, ll] = size(lambda)
 [t,tt] = size(theta)
