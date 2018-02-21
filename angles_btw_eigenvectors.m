503167dec2b55055fa87168e9fb0fd5e89bbc0a3
Lydia <lydia.pogorelskaya@gmail.com>
2018-02-21 18:17:43 +0300

    right angles!

--- angles_btw_eigenvectors.m
+++ angles_btw_eigenvectors.m
@@ -14,24 +14,25 @@ for nvector=1:450
     u(:) = H_perturbed_sorted(1,1,:,nvector);
     v(:) = H_nonperturbed_sorted(1,1,:,nvector);
     %cos_theta = dot(u,v)/(norm(u)*norm(v));
-    cos_theta = dot(u,conj(v))/( sqrt ( dot(u,conj(u)) )*sqrt( dot(v,conj(v)) ) );
-    theta_btw_perturbed_nonperturbed(nvector) = acosd(cos_theta);
+    cos_theta = (sum(u.*conj(v)))/( sqrt ( dot(u,u) )*sqrt( dot(v,v) ) );
+    theta_btw_perturbed_nonperturbed(nvector) = acosd(abs(cos_theta));
 end
 
 for nvector=1:449
     u(:) = H_perturbed_sorted(1,1,:,nvector);
     v(:) = H_perturbed_sorted(1,1,:,nvector+1);
     %cos_theta = dot(u,v)/(norm(u)*norm(v));
-    cos_theta = dot(u,conj(v))/( sqrt ( dot(u,conj(u)) )*sqrt( dot(v,conj(v)) ) );
-    theta_btw_perturbed_neighbour(nvector) = acosd(cos_theta);
+    cos_theta = (sum(u.*conj(v)))/( sqrt ( dot(u,u) )*sqrt( dot(v,v) ) );
+    theta_btw_perturbed_neighbour(nvector) = acosd(abs(cos_theta));
     
     u(:) = H_nonperturbed_sorted(1,1,:,nvector);
     v(:) = H_nonperturbed_sorted(1,1,:,nvector+1);
     %cos_theta = dot(u,v)/(norm(u)*norm(v));
-    cos_theta = dot(u,conj(v))/( sqrt ( dot(u,conj(u)) )*sqrt( dot(v,conj(v)) ) );
-    theta_btw_nonperturbed_neighbour(nvector) = acosd(cos_theta);
+    cos_theta =(sum(u.*conj(v)))/( sqrt ( dot(u,u)) )*sqrt( dot(v,v)  );
+    theta_btw_nonperturbed_neighbour(nvector) = acosd(abs(cos_theta));
+    cos_nonperturbed(nvector) = cos_theta
 end
-
+abs(cos_nonperturbed)
 
 gammasqr_nonperturbed(:,1) = gammasqr_nonperturbed_sorted(1,1,:);
 gammasqr_perturbed(:,1) = gammasqr_perturbed_sorted(1,1,:);
